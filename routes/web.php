4171acfac5fd0b0f6f76093f35cff6d4fce7d8f8
yoganaufalrp <50273134+yoganaufalrayputro@users.noreply.github.com>
2021-12-22 21:28:24 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('posts', PostController::class);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/home.index', [
+        "title"=> "Home"
+    ]);
 });
+
+#Login
+Route::get('/login', [LoginController::class, 'index']) -> middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+#Logout
+Route::post('logout',[LoginController::class,'logout']);
+
+#Register
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
